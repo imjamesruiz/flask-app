b5088a66531ace073073e332df76b4f79ab9c603
imjamesruiz <jamesruiz610@gmail.com>
2025-08-14 19:11:23 -0700

    added basic home page before with simple styling before bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") # main route to home page ; empty path
-def hello_world():
-    return "<h1>Hello, world!</h1>"
+def hello_jovian():
+    return render_template("index.html")
 
 
